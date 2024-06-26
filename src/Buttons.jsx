1165706818a7e4f383f7fa8ae18885864e7b8353
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-06-26 13:54:19 -0400

    buttons.jsx import

---
+++ src/Buttons.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import moogleLeft from "./img/mooglepointingleft.png";
+import moogleRight from "./img/mooglepointingright.png";
+import randomButton from "./img/shuffleicon.png";
