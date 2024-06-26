0c0fa19d8fd5890b43fa26eabd0cfd074c363112
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-06-26 14:14:03 -0400

    leftButton rightButton

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -2,3 +2,16 @@ import React from "react";
 import moogleLeft from "./img/mooglepointingleft.png";
 import moogleRight from "./img/mooglepointingright.png";
 import randomButton from "./img/shuffleicon.png";
+
+function Buttons(props) {
+  return (
+    <div className="buttons">
+      <button className="leftButton" onClick={props.decrement}>
+        <img src={moogleLeft} className="buttonImg" alt="leftarrow" />
+      </button>
+      <button className="rightButton" onClick={props.increment}>
+        <img src={moogleRight} className="buttonImg" alt="rightarrow" />
+      </button>
+    </div>
+  )
+}
