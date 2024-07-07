e31de3b2993c2530f6781f432cd13eaa3a3fd69f
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-07-07 16:21:12 -0400

    function image

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,2 +1,10 @@
 import React from "react";
 import missingPic from "./img/imageavailable.png"
+
+function Image(props) {
+  const theImage = props.characters && props.characters[props.index].pictures[0]
+    ? props.characters[props.index].pictures[0].url
+    : missingPic
+}
+
+export default Image
