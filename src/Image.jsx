0271d9d7c7d51ee72a35b2cd626720be3a0a08a6
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-07-07 16:39:21 -0400

    return ()

--- src/Image.jsx
+++ src/Image.jsx
@@ -5,6 +5,14 @@ function Image(props) {
   const theImage = props.characters && props.characters[props.index].pictures[0]
     ? props.characters[props.index].pictures[0].url
     : missingPic
+  
+    return (
+      <div className="slides">
+      {props.characters && (
+        <img className="charImage" src={theImage} alt="charImage" />
+      )}
+      </div>
+    )
 }
 
 export default Image
